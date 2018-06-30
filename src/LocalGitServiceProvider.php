74e554f0322b13b1f906667360d9bd8b2f389c4b
sebsel <sebsel@seblog.nl>
2018-06-30 08:26:29 +0200

    add Laravel ServiceProvider

---
+++ src/LocalGitServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Sebsel\Flysystem\LocalGit;
+
+use Illuminate\Filesystem\FilesystemManager;
+use Illuminate\Support\ServiceProvider;
+use League\Flysystem\Filesystem;
+
+class LocalGitServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     * @return void
+     */
+    public function boot()
+    {
+        $factory = $this->app->make('filesystem');
+
+        $factory->extend('local-git', function ($app, $config) {
+            return new Filesystem(new LocalGitAdapter($config['root']));
+        });
+    }
+}
